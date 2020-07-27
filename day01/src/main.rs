0e7e8b650a0a2d7b238a0a7440f7aeb5bd02acb0
IamDLY <1219642339@qq.com>
2020-07-27 22:17:35 +0800

    基础的创建窗体，设置一个背景色，画一个三角形

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,17 +1,92 @@
+#[macro_use]
 extern crate glium;
 
 use glium::glutin;
 use glium::Surface;
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
 fn main() {
-    let mut events_loop = glutin::event_loop::EventLoop::new();
+    let events_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).unwrap();
 
-    let mut closed = false;
+    let v1 = Vertex {
+        position: [0.5, 0.5],
+    };
+    let v2 = Vertex {
+        position: [0.5, -0.5],
+    };
+    let v3 = Vertex {
+        position: [-0.5, -0.5],
+    };
+    // let v4 = Vertex {
+    //     position: [-0.5, 0.5],
+    // };
+    let shape = vec![v1, v2, v3];
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-    while !closed {
-        events_loop.run(move |ev, _, control_flow| {});
-    }
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(0.0, 1.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
+
+    events_loop.run(move |ev, _, control_flow| {
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => {}
+            },
+            _ => (),
+        }
+
+        let mut target = display.draw();
+
+        target.clear_color(1.0, 0.0, 0.0, 1.0);
+
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+
+        target.finish().unwrap();
+    });
 }
